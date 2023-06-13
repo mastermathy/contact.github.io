d5954da79aa9945cf5c57dfe3703732705422338
Mathias Eju <mathiaseju@gmail.com>
2023-06-13 16:19:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var popup = document.querySelector(".popup");
+    var closeButton = document.querySelector(".close");
+  
+    closeButton.addEventListener("click", function() {
+      popup.style.display = "none";
+    });
+  
+    // Show the popup after a delay (e.g., 2 seconds)
+    setTimeout(function() {
+      popup.style.display = "flex";
+    }, 2000);
+  });
